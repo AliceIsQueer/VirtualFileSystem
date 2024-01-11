7d16f99d56cb83fff700386334d79540a2853621
Alicja <alicjakruszewicz3@gmail.com>
2024-01-11 12:31:41 +0100

    Add interactive mode.

--- src/main.cpp
+++ src/main.cpp
@@ -18,138 +18,144 @@ int main(int argc, char *argv[]) {
 		filesystemName = argv[1];
 	}
 
+	bool isInteractive = (argc <= 1);
+
 	int choice;
-	if (argc <= 2)  {
-		std::cout << "1 - createFilesystem\n2 - copyFileTo\n3 - copyFileFrom\n4 - ls\n5 - rm\n6 - info\n7 - deleteFilesystem\n";
-		std::cin >> choice;
-	}
-	else {
-		choice = std::stoi(argv[2]);
-	}
-	
-	if (choice == 1) {
-		int size;
+	do {
+		if (argc <= 2)  {
+			std::cout << "1 - createFilesystem\n2 - copyFileTo\n3 - copyFileFrom\n4 - ls\n5 - rm\n6 - info\n7 - deleteFilesystem\n";
+			if (isInteractive)
+				std::cout << "8 - exit\n";
 
-		if (argc <= 3) {
-			std::cout << "Please input the size of the file system in Bytes \n";
-			std::cin >> size;
+			std::cin >> choice;
 		}
 		else {
-			size = std::stoi(argv[3]);
+			choice = std::stoi(argv[2]);
 		}
 
-		createFilesystem(size, filesystemName);
-		return 0;
-	}
+		if (choice == 1) {
+			int size;
 
-	FILE *tempFile = fopen(filesystemName.c_str(), "r");
-	if (tempFile == NULL) {
-		std::cout << "Given filesystem does not exist" << std::endl;
-		return 1;
-	}
-	fclose(tempFile);
+			if (argc <= 3) {
+				std::cout << "Please input the size of the file system in Bytes \n";
+				std::cin >> size;
+			}
+			else {
+				size = std::stoi(argv[3]);
+			}
 
-	if (choice == 2) {
-		std::string fileName;
-		std::string newFileName;
-		if (argc <= 3) {
-			std::cout << "Please input the name of the file you wish to copy as well as its new filename\n";
-			std::cin >> fileName >> newFileName;
-		} 
-		if (argc <= 4) {
-			fileName = argv[3];
-			newFileName = argv[3];
-		}
-		else {
-			fileName = argv[3];
-			newFileName = argv[4];
+			createFilesystem(size, filesystemName);
 		}
 
-		FILE *fileSystem;
-		FILE *file;
+		FILE *tempFile = fopen(filesystemName.c_str(), "r");
+		if (tempFile == NULL) {
+			std::cout << "Given filesystem does not exist" << std::endl;
+			return 1;
+		}
+		fclose(tempFile);
 
-		fileSystem = fopen(filesystemName.c_str(), "rb+");
-		file = fopen(fileName.c_str(), "r");
+		if (choice == 2) {
+			std::string fileName;
+			std::string newFileName;
 
-		copyTo(fileSystem, file, newFileName);
+			if (argc <= 3) {
+				std::cout << "Please input the name of the file you wish to copy as well as its new filename\n";
+				std::cin >> fileName >> newFileName;
+			} 
+			else if (argc <= 4) {
+				fileName = argv[3];
+				newFileName = argv[3];
+			}
+			else {
+				fileName = argv[3];
+				newFileName = argv[4];
+			}
+            std::cout << fileName << " " << newFileName << std::endl;
 
-		fclose(fileSystem);
-		fclose(file);
-		return 0;
-	}	
-	if (choice == 3) {
-		std::string fileName;
-		std::string virtualFileName;
+			FILE *file;
+			FILE *fileSystem;
 
-		FILE *fileSystem;
-		fileSystem = fopen(filesystemName.c_str(), "r");
+			file = fopen(fileName.c_str(), "r");
+			fileSystem = fopen(filesystemName.c_str(), "rb+");
 
-		if (argc <= 3) {
-			std::cout << "Please input the name of the file you wish to copy from filesystem, as well the new filename:\n";
+			copyTo(fileSystem, file, newFileName);
+
+			fclose(fileSystem);
+			fclose(file);
+		}	
+		if (choice == 3) {
+			std::string fileName;
+			std::string virtualFileName;
+
+			FILE *fileSystem;
+			fileSystem = fopen(filesystemName.c_str(), "r");
+
+			if (argc <= 3) {
+				std::cout << "Please input the name of the file you wish to copy from filesystem, as well the new filename:\n";
+				ls(fileSystem);
+				std::cin >> virtualFileName >> fileName;
+			}
+			else if (argc <= 4) {
+				virtualFileName = argv[3];
+				fileName = argv[3];
+			}
+			else {
+				virtualFileName = argv[3];
+				fileName = argv[4];
+			}
+
+
+
+			FILE *file;
+			file = fopen(fileName.c_str(), "wb+");
+
+			copyFrom(fileSystem, file, virtualFileName);
+
+			fclose(fileSystem);
+			fclose(file);
+		}
+		if (choice == 4) {
+			FILE *fileSystem;
+			fileSystem = fopen(filesystemName.c_str(), "r");
+
 			ls(fileSystem);
-			std::cin >> virtualFileName >> fileName;
+
+			fclose(fileSystem);
 		}
-		else if (argc <= 4) {
-			virtualFileName = argv[3];
-			fileName = argv[3];
+		if (choice == 5) {
+			FILE *fileSystem;
+
+			fileSystem = fopen(filesystemName.c_str(), "rb+");
+
+			std::string fileName;
+			
+			if(argc <= 3) {
+				std::cout << "Please input the name of the file you wish to delete:\n";
+				ls(fileSystem);
+				std::cin >> fileName;
+			}
+			else {
+				fileName = argv[3];
+			}
+
+			rm(fileSystem, fileName);
+
+			fclose(fileSystem);
 		}
-		else {
-			virtualFileName = argv[3];
-			fileName = argv[4];
+		if (choice == 6) {
+			FILE *fileSystem;
+
+			fileSystem = fopen(filesystemName.c_str(), "r");
+
+			info(fileSystem);
 		}
-
-
-
-		FILE *file;
-		file = fopen(fileName.c_str(), "wb+");
-
-		copyFrom(fileSystem, file, virtualFileName);
-
-		fclose(fileSystem);
-		fclose(file);
-		return 0;
-	}
-	if (choice == 4) {
-		FILE *fileSystem;
-		fileSystem = fopen(filesystemName.c_str(), "r");
-
-		ls(fileSystem);
-
-		fclose(fileSystem);
-		return 0;
-	}
-	if (choice == 5) {
-		FILE *fileSystem;
-
-		fileSystem = fopen(filesystemName.c_str(), "rb+");
-
-		std::string fileName;
-		
-		if(argc <= 3) {
-			std::cout << "Please input the name of the file you wish to delete:\n";
-			ls(fileSystem);
-			std::cin >> fileName;
-		}
-		else {
-			fileName = argv[3];
+		if (choice == 7) {
+			remove(filesystemName.c_str());
 		}
 
-		rm(fileSystem, fileName);
+		if(isInteractive)
+			std::cout << "\n\n\n";
+	} while(isInteractive && choice != 8);
 
-		fclose(fileSystem);
-		return 0;
-	}
-	if (choice == 6) {
-		FILE *fileSystem;
-
-		fileSystem = fopen(filesystemName.c_str(), "r");
-
-		info(fileSystem);
-		return 0;
-	}
-	if (choice == 7) {
-		remove(filesystemName.c_str());
-		return 0;
-	}
 	return 0;
 }
